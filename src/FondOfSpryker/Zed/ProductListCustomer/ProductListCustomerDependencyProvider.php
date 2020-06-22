197838c96bb6a4c6f05d76f78765d7360bde8aeb
Jozsef Geng <gengjozsef86@gmail.com>
2020-06-22 14:08:48 +0200

    add Product List Customer relation Post Save Plugins

--- src/FondOfSpryker/Zed/ProductListCustomer/ProductListCustomerDependencyProvider.php
+++ src/FondOfSpryker/Zed/ProductListCustomer/ProductListCustomerDependencyProvider.php
@@ -9,6 +9,7 @@ use Spryker\Zed\Kernel\Container;
 class ProductListCustomerDependencyProvider extends AbstractBundleDependencyProvider
 {
     public const PROPEL_QUERY_PRODUCT_LIST = 'PROPEL_QUERY_PRODUCT_LIST';
+    public const PLUGINS_PRODUCT_LIST_CUSTOMER_RELATION_POST_SAVE = 'PLUGINS_PRODUCT_LIST_CUSTOMER_RELATION_POST_SAVE';
 
     /**
      * @param \Spryker\Zed\Kernel\Container $container
@@ -28,6 +29,19 @@ class ProductListCustomerDependencyProvider extends AbstractBundleDependencyProv
      *
      * @return \Spryker\Zed\Kernel\Container
      */
+    public function provideBusinessLayerDependencies(Container $container): Container
+    {
+        $container = parent::provideBusinessLayerDependencies($container);
+        $container = $this->addProductListCustomerPostSavePlugins($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
     protected function addProductListPropelQuery(Container $container): Container
     {
         $container[static::PROPEL_QUERY_PRODUCT_LIST] = function (Container $container) {
@@ -35,5 +49,29 @@ class ProductListCustomerDependencyProvider extends AbstractBundleDependencyProv
         };
 
         return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addProductListCustomerPostSavePlugins(Container $container): Container
+    {
+        $container[static::PLUGINS_PRODUCT_LIST_CUSTOMER_RELATION_POST_SAVE] = function () {
+            return $this->getProductListCustomerRelationPostSavePlugins();
+        };
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \FondOfSpryker\Zed\ProductListCustomerExtension\Dependency\Plugin\ProductListCustomerPostSavePluginInterface[]
+     */
+    protected function getProductListCustomerRelationPostSavePlugins()
+    {
+        return [];
     }
 }
